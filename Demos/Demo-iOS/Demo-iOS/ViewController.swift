7f7e72ae7a7b9c672eaa7f2d0869e9244a19ceca
Hardik <hardikdg.sa@gmail.com>
2018-01-03 08:58:10 +0530

    Updated frame

--- Demos/Demo-iOS/Demo-iOS/ViewController.swift
+++ Demos/Demo-iOS/Demo-iOS/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
         let url = Bundle.main.url(forResource: "tiger", withExtension: "svg")!
 
         //initialise a view that parses and renders an SVG file in the bundle:
-        let svgImageView = SVGImageView.init(svgSource: "<?xml version=\"1.0\" encoding=\"UTF-8\"?><svg width=\"25.000000pt\" height=\"25.000000pt\" viewBox=\"0 0 25.000000 25.000000\" preserveAspectRatio=\"xMidYMid meet\"><g transform=\"translate(0.000000,25.000000)\" fill=\"#00ffff\" stroke=\"none\"><path d=\"M169 211 c-20 -22 -47 -58 -60 -80 -21 -38 -49 -55 -49 -31 0 14 -22 12 -42 -4 -16 -12 -15 -16 13 -54 42 -57 68 -56 93 4 20 48 74 132 107 165 25 25 24 39 -3 39 -12 0 -38 -17 -59 -39z\"/></g></svg>")
+        let svgImageView = SVGImageView.init(svgSource: "<?xml version=\"1.0\" encoding=\"UTF-8\"?><svg width=\"25.000000pt\" height=\"25.000000pt\" viewBox=\"0 0 25.000000 25.000000\" preserveAspectRatio=\"xMidYMid meet\"><g transform=\"translate(0.000000,25.000000) scale(0.10000,-0.10000)\" fill=\"#00ffff\" stroke=\"none\"><path d=\"M169 211 c-20 -22 -47 -58 -60 -80 -21 -38 -49 -55 -49 -31 0 14 -22 12 -42 -4 -16 -12 -15 -16 13 -54 42 -57 68 -56 93 4 20 48 74 132 107 165 25 25 24 39 -3 39 -12 0 -38 -17 -59 -39z\"/></g></svg>")
         
 //        let svgImageView = SVGImageView.init(contentsOf: url)
 
@@ -30,12 +30,14 @@ class ViewController: UIViewController {
 
 
         //layout the view:
-        svgImageView.translatesAutoresizingMaskIntoConstraints = false
+//        svgImageView.translatesAutoresizingMaskIntoConstraints = false
+        svgImageView.frame = CGRect(x: 20, y: 80, width: 50, height: 50)
         view.addSubview(svgImageView)
 
-        svgImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        svgImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        svgImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
+//        svgImageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        svgImageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        svgImageView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         
     }
 }
